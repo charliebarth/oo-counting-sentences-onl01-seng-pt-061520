32a8ba57e1e9c850f8c9dcd5a4a0053645b57646
Charlie Barth <charliepbarth@gmail.com>
2020-07-06 18:32:43 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -2,12 +2,5 @@ text = "Hipsters are everywhere! Even in Home Depot? Flannel wrench set in ma fa
 
 result = text.split(/[.?!]/)
 
-result = text.split(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
-new_array = []
-result.each do |num|
-  count = result.index("#{num}")
-  if num == " "
-    result.delete!(count)
-  end
-end
+
 puts result
