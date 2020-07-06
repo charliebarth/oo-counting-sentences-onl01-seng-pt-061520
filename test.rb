4dbedbab7e3126ccd2ce391598050a4325fab6f3
Charlie Barth <charliepbarth@gmail.com>
2020-07-06 18:08:28 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,2 +1,5 @@
-string = "Hi. My name is Charlie! This is a test."
-puts string.split
+text = "Hipsters are everywhere! Even in Home Depot? Flannel wrench set in ma face."
+
+result = text.split(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
+
+puts result
