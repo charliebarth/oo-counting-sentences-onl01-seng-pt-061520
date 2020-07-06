a5bfd7efc24f6abcc4597b6e17e8ab3bddc53a2c
Charlie Barth <charliepbarth@gmail.com>
2020-07-06 18:22:58 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,3 +3,4 @@ text = "Hipsters are everywhere! Even in Home Depot? Flannel wrench set in ma fa
 result = text.split(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
 
 puts result.delete(" ")
+puts result
