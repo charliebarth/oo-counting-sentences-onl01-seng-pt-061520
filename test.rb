9d1c9baf9f036e8144ec61cc904876e4dc29a4f7
Charlie Barth <charliepbarth@gmail.com>
2020-07-06 18:28:46 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,11 +1,11 @@
 text = "Hipsters are everywhere! Even in Home Depot? Flannel wrench set in ma face. Sup my dude. What's goign on?"
 
 result = text.split(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
-
+new_array = []
 result.each do |num|
   count = result[num]
   if num == " "
-    result.delete(count)
+    result.delete!(count)
   end
 end
 puts result.size
