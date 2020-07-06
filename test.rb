cb7303b188bcab198b18488d06a404f9f8697f3b
Charlie Barth <charliepbarth@gmail.com>
2020-07-06 18:17:27 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -5,4 +5,4 @@ result = text.split(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
 puts result
 new_string = result.join("|")
 new_array = new_string.split("|")
-puts new_array.size - 2
+puts new_array.size 
