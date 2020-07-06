0ab6c171a0d2fb39da6ce59c0a234c84a6aa4d9f
Charlie Barth <charliepbarth@gmail.com>
2020-07-06 18:37:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,13 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".")
-    true
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-    self.end_with?("?")
-    true
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
